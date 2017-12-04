884a0bb3e51fce64b757c71e2e98d2f9e128d168
Jacek Andrzejewski <jacekandrzejewskiit@gmail.com>
2017-12-04 06:17:50 +0100

    Reworked game with states Created mockup title menu, game over screen and game win screen

--- js/game.js
+++ js/game.js
@@ -1,235 +1,361 @@
 
-
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'My App', { preload: preload, create: create, update: update });
-
-function preload() {
-	game.load.image('tile', 'assets/images/tile.png');
-	game.load.image('win', 'assets/images/win.png');
-	game.load.image('button', 'assets/images/button.png');
-	game.load.image('buttonclicked', 'assets/images/buttonclicked.png');
-	game.load.image('closeddoor1', 'assets/images/closeddoor1.png');
-	game.load.image('closeddoor2', 'assets/images/closeddoor2.png');
-	game.load.image('openeddoor1', 'assets/images/openeddoor1.png');
-	game.load.image('openeddoor2', 'assets/images/openeddoor2.png');
-	game.load.tilemap('map', 'assets/maps/level1.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.image('background', 'assets/images/sky.png');
-	game.load.spritesheet('player', 'assets/images/player.png', 64, 96);
-	game.load.spritesheet('coin', 'assets/images/coins.png', 32, 32);
-	game.load.image('heart', 'assets/images/heart.png');
-	game.load.image('expl', 'assets/images/expl.png');
-}
-
-var bg;
-var facing = 'turn';
-var jumpTimer = 0;
-var map;
-var layer;
-var player;
-var cursors;
-var jumpButton;
-var enemy;
-var enemySpeed = 100;
-var score = 0;
-var scoreText;
-var lifes;
-var coins;
-var gravity = 400;
-
-function create() {
-
-	//conf
-  game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.time.desiredFps = 60;
-
-	//background
-  bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-  bg.fixedToCamera = true;
-
-	//map
-	map = game.add.tilemap('map');
-	map.addTilesetImage('tile');
-	map.addTilesetImage('win');
-	map.addTilesetImage('button');
-	map.addTilesetImage('buttonclicked');
-	map.addTilesetImage('closeddoor1');
-	map.addTilesetImage('closeddoor2');
-	map.addTilesetImage('openeddoor1');
-	map.addTilesetImage('openeddoor2');
-
-	map.setCollisionByExclusion([0,2,3,7,8]);
-	layer = map.createLayer('layer01');
-
-	layer.resizeWorld();
-	//layer.debug=true;
-
-	//show score
-	scoreText = game.add.text(16, 16, 'Score: '+ score, { fontSize: '32px', fill: '#000' });
-
-	//show lifes
-	lives = game.add.group();
-	game.add.text(320, 16, 'Lives : ', { fontSize: '32px', fill: '#fff' });
-	for (var i = 0; i < 3; i++)
-    {
-        var heart = lives.create(432 + (32 * i), 22, 'heart');
-        //heart.anchor.setTo(0.5, 0.5);
-    }
-
-	//coins
-	coins = game.add.group();
-	coins.create(96,96, 'coin',0);
-	coins.create(0,352, 'coin',0);
-	coins.create(32,448, 'coin',0);
-	coins.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3, 4, 5], 10, true);
-	coins.callAll('animations.play', 'animations', 'spin');
-	//coins.scale.set(0.7,0.7);
-	coins.enableBody = true;
-	game.physics.enable(coins, Phaser.Physics.ARCADE);
-	coins.setAll('body.gravity.y',-gravity);
-
-	//gravity
-	game.physics.arcade.gravity.y = gravity;
-
-	//player
-	makePlayer();
-
-	//movement
-	cursors = game.input.keyboard.createCursorKeys();
-  jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-	//enemy
-	enemy = game.add.sprite(32, 448, 'expl');
-	game.physics.enable(enemy, Phaser.Physics.ARCADE);
-	enemy.body.collideWorldBounds = true;
-	enemy.body.velocity.x=100;
-	enemy.scale.set(0.7,0.7);
-
-	//set callback if
-	map.setTileIndexCallback([2], function(){win();}, this);
-	map.setTileIndexCallback([3], function(){buttonClicked();}, this);
-}
-
-function update() {
-
-	//collisions
-	game.physics.arcade.collide(player,layer);
-	game.physics.arcade.collide(enemy,layer);
-	game.physics.arcade.collide(coins,layer);
-
-	//check collisions
-	game.physics.arcade.overlap(player,enemy, restart);
-	game.physics.arcade.overlap(player, coins, collectCoin);
-
-	//player movement
-	player.body.velocity.x = 0;
-	if (cursors.left.isDown)
-  {
-    player.body.velocity.x = -150;
-
-    if (facing != 'left')
-    {
-        player.animations.play('left');
-        facing = 'left';
-    }
-  }
-  else if (cursors.right.isDown)
-  {
-    player.body.velocity.x = 150;
-
-    if (facing != 'right')
-    {
-        player.animations.play('right');
-        facing = 'right';
-    }
-  }
-  else
-  {
-      if (facing != 'idle')
-      {
-          player.animations.stop();
-
-          player.frame = 2;
-          facing = 'idle';
-      }
-    }
-
-	if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
-  {
-		player.body.velocity.y = -350;
-    jumpTimer = game.time.now + 500;
-  }
-
-	//enemy movement
-	if(enemy.body.blocked.right && enemy.body.blocked.down)
+class Boot extends Phaser.State
+{
+	preload()
 	{
-		enemy.body.velocity.x=-enemySpeed;
+		console.log("Boot preload");
 	}
-	if(enemy.body.blocked.left && enemy.body.blocked.down)
+
+	create()
 	{
-		enemy.body.velocity.x=enemySpeed;
+		console.log("Boot create");
 	}
 
-}
-
-function makePlayer()
-{
-	player = game.add.sprite(16, 32, 'player');
-	game.physics.enable(player, Phaser.Physics.ARCADE);
-
-	player.body.collideWorldBounds = true;
-	player.body.setSize(32, 48, 16, 48);
-
-	player.animations.add('left', [4, 5, 6, 7], 10, true);
-  player.animations.add('turn', [2], 20, true);
-  player.animations.add('right', [8, 9, 10, 11], 10, true);
-}
-
-function restart()
-{
-
-	live = lives.getChildAt(lives.countLiving()-1);
-	live.kill();
-	player.kill();
-
-	if (lives.countLiving() < 1)
-  {
-		console.log('lose');
-	}
-	else
+	update()
 	{
-		console.log('restart');
-		makePlayer();
+		console.log("Boot update");
+		this.game.state.start("Preload");
 	}
-	//game.state.restart();
 }
 
-function win()
+class Preload extends Phaser.State
 {
-	console.log('win');
-	game.state.restart();
+	preload()
+	{
+		console.log("Preload preload");
+		this.game.load.image("play","assets/images/play.jpg");
+	}
+
+	create()
+	{
+		console.log("Preload create");
+	}
+
+	update()
+	{
+		console.log("Preload update");
+		this.game.state.start("Title");
+	}
 }
 
-function buttonClicked()
+class Title extends Phaser.State
 {
-	console.log('button');
-	map.replace(3,4);
-	doorOpen();
+	preload()
+	{
+		console.log("Title preload");
+	}
+
+	create()
+	{
+		console.log("Title create");
+		this.start=false;
+		this.game.add.button(0,0,"play",()=>{this.start=true;});
+	}
+
+	update()
+	{
+		console.log("Title update");
+		if(this.start===true)
+			this.game.state.start("Main");
+	}
 }
 
-function doorOpen()
+class Main extends Phaser.State
 {
-	console.log('door');
-	map.replace(5,7);
-	map.replace(6,8);
-	map.setCollision(7,false,layer);
-	map.setCollision(8,false,layer);
+	preload()
+	{
+		console.log("Main preload");
+		this.game.load.image('tile', 'assets/images/tile.png');
+		this.game.load.image('win', 'assets/images/win.png');
+		this.game.load.image('button', 'assets/images/button.png');
+		this.game.load.image('buttonclicked', 'assets/images/buttonclicked.png');
+		this.game.load.image('closeddoor1', 'assets/images/closeddoor1.png');
+		this.game.load.image('closeddoor2', 'assets/images/closeddoor2.png');
+		this.game.load.image('openeddoor1', 'assets/images/openeddoor1.png');
+		this.game.load.image('openeddoor2', 'assets/images/openeddoor2.png');
+		this.game.load.tilemap('map', 'assets/maps/level1.json', null, Phaser.Tilemap.TILED_JSON);
+		this.game.load.image('background', 'assets/images/sky.png');
+		this.game.load.spritesheet('player', 'assets/images/player.png', 64, 96);
+		this.game.load.spritesheet('coin', 'assets/images/coins.png', 32, 32);
+		this.game.load.image('heart', 'assets/images/heart.png');
+		this.game.load.image('expl', 'assets/images/expl.png');
+	}
+
+	create()
+	{
+		console.log("Main create");
+		//game configuration
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
+		this.game.time.desiredFps = 60;
+		console.log("Game configured");
+
+		//background settings
+		this.bg=this.game.add.tileSprite(0, 0, 800, 600, 'background');
+		this.bg.fixedToCamera = true;
+		console.log("Background set");
+
+		//map images loading
+		this.map=this.game.add.tilemap('map');
+		this.map.addTilesetImage('tile');
+		this.map.addTilesetImage('win');
+		this.map.addTilesetImage('button');
+		this.map.addTilesetImage('buttonclicked');
+		this.map.addTilesetImage('closeddoor1');
+		this.map.addTilesetImage('closeddoor2');
+		this.map.addTilesetImage('openeddoor1');
+		this.map.addTilesetImage('openeddoor2');
+		this.map.setCollisionByExclusion([0,2,3,7,8]);
+		console.log("Map loaded");
+
+		//creating layer of map
+		this.layer=this.map.createLayer('layer01');
+		this.layer.resizeWorld();
+		console.log("Layers created");
+
+		//setting constants
+		this.gravity=400;
+		this.enemySpeed=100;
+		this.maxLives=3;
+		console.log("Constants set");
+
+		//setting variables
+		this.facing='turn';
+		this.jumpTimer=0;
+		this.score=0;
+		this.gameOver=false;
+		this.updates=0;
+		console.log("Variables set");
+
+		//setting hud elements
+		this.scoreText=this.game.add.text(16,16,'Score: '+this.score,{fontSize:'32px',fill:'#000'});
+		this.lives=this.game.add.group();
+		this.game.add.text(320,16,'Lives: ',{fontSize:'32px',fill:'#fff'});
+		for(let i=0;i<this.maxLives;i++)
+			this.heart=this.lives.create(432+(32*i),22,'heart');
+		console.log("Hud set",this.scoreText);
+
+		//setting map elements
+		this.coins=this.game.add.group();
+		this.coins.create(96,96, 'coin',0);
+		this.coins.create(0,352, 'coin',0);
+		this.coins.create(32,448, 'coin',0);
+		this.enemy=this.game.add.sprite(32, 448, 'expl');
+		console.log("Map elements set");
+
+		//enabling animations
+		this.coins.callAll('animations.add','animations','spin', [0, 1, 2, 3, 4, 5], 10, true);
+		this.coins.callAll('animations.play','animations','spin');
+		console.log("Animations set");
+
+		//enabling controls
+		this.cursors=this.game.input.keyboard.createCursorKeys();
+		this.jumpButton=this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		console.log("Controls set");
+
+		//enabling physics for elements
+		this.game.physics.arcade.gravity.y=this.gravity;
+		console.log("Physics enabled");
+
+		//enemy physics
+		this.game.physics.enable(this.enemy, Phaser.Physics.ARCADE);
+		this.enemy.body.collideWorldBounds = true;
+		this.enemy.body.velocity.x=100;
+		this.enemy.scale.set(0.7,0.7);
+		console.log("Enemy physics set");
+
+		//player creation
+		this.makePlayer();
+		console.log("Player physics set");
+
+		//coins physics
+		this.game.physics.enable(this.coins,Phaser.Physics.ARCADE);
+		this.coins.enableBody=true;
+		this.coins.setAll('body.gravity.y',-(this.gravity));
+		console.log("Coin physics set");
+
+		//set map elements callbacks
+		this.map.setTileIndexCallback([2], ()=>{this.game.state.start("GameWon");}, this);
+		this.map.setTileIndexCallback([3], ()=>{this.buttonClicked();}, this);
+		console.log("Map callbacks set");
+	}
+
+	update()
+	{
+		//collisions
+		this.game.physics.arcade.collide(this.player,this.layer);
+		this.game.physics.arcade.collide(this.enemy,this.layer);
+		this.game.physics.arcade.collide(this.coins,this.layer);
+
+		//check collisions
+		this.game.physics.arcade.overlap(this.player,this.enemy,this.restart,null,this);
+		this.game.physics.arcade.overlap(this.player,this.coins,this.collectCoin,null,this);
+
+		//player movement
+		this.player.body.velocity.x = 0;
+		if (this.cursors.left.isDown)
+		{
+			this.player.body.velocity.x = -150;
+
+			if (this.facing != 'left')
+			{
+				this.player.animations.play('left');
+				this.facing = 'left';
+			}
+		}
+		else if (this.cursors.right.isDown)
+		{
+			this.player.body.velocity.x = 150;
+
+			if (this.facing != 'right')
+			{
+				this.player.animations.play('right');
+				this.facing = 'right';
+			}
+		}
+		else
+		{
+			if (this.facing != 'idle')
+			{
+				this.player.animations.stop();
+
+				this.player.frame = 2;
+				this.facing = 'idle';
+			}
+		}
+
+		if (this.jumpButton.isDown && this.player.body.onFloor() && this.game.time.now > this.jumpTimer)
+		{
+			this.player.body.velocity.y = -350;
+			this.jumpTimer = game.time.now + 500;
+		}
+
+		//enemy movement
+		if(this.enemy.body.blocked.right && this.enemy.body.blocked.down)
+		{
+			this.enemy.body.velocity.x=-this.enemySpeed;
+		}
+		if(this.enemy.body.blocked.left && this.enemy.body.blocked.down)
+		{
+			this.enemy.body.velocity.x=this.enemySpeed;
+		}
+
+		console.log("Main update");
+		if(this.gameOver===true)
+			this.game.state.start("GameOver");
+	}
+
+	buttonClicked()
+	{
+		this.map.replace(3,4);
+		this.doorOpen();
+	}
+
+	collectCoin(player, coin)
+	{
+		coin.kill();
+		this.score+=10;
+		this.scoreText.text='Score: '+this.score;
+	}
+
+	doorOpen()
+	{
+		this.map.replace(5,7);
+		this.map.replace(6,8);
+		this.map.setCollision(7,false,this.layer);
+		this.map.setCollision(8,false,this.layer);
+	}
+
+	makePlayer()
+	{
+		this.player = this.game.add.sprite(16, 32, 'player');
+		this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+		this.player.body.collideWorldBounds = true;
+		this.player.body.setSize(32, 48, 16, 48);
+
+		this.player.animations.add('left', [4, 5, 6, 7], 10, true);
+		this.player.animations.add('turn', [2], 20, true);
+		this.player.animations.add('right', [8, 9, 10, 11], 10, true);
+	}
+
+	restart()
+	{
+		this.live=this.lives.getChildAt(this.lives.countLiving()-1);
+		this.live.kill();
+		this.player.kill();
+
+		if (this.lives.countLiving() < 1)
+		{
+			console.log('lose');
+		}
+		else
+		{
+			console.log('restart');
+			this.makePlayer();
+		}
+	}
 }
 
-function collectCoin (player, coin) {
+class GameOver extends Phaser.State
+{
+	preload()
+	{
+		console.log("GameOver preload");
 
-    // Removes the star from the screen
-    coin.kill();
+	}
 
-    score += 10;
-    scoreText.text = 'Score: ' + score;
+	create()
+	{
+		console.log("GameOver create");
+		this.game.add.button(0,0,"play",()=>{this.restart=true;});
+		this.restart=false;
+	}
 
+	update()
+	{
+		console.log("GameOver update");
+		if(this.restart===true)
+			this.game.state.start("Title");
+	}
 }
+
+class GameWon extends Phaser.State
+{
+	preload()
+	{
+		console.log("GameWon preload");
+	}
+
+	create()
+	{
+		console.log("GameWon create");
+		this.game.add.button(0,0,"play",()=>{this.restart=true;});
+		this.restart=false;
+	}
+
+	update()
+	{
+		console.log("GameWon update");
+		if(this.restart===true)
+			this.game.state.start("Title");
+	}
+}
+
+class Game extends Phaser.Game
+{
+	constructor()
+	{
+		super('100%','100%',Phaser.AUTO,'');
+
+		this.state.add('Boot', Boot, false);
+		this.state.add('Preload', Preload, false);
+		this.state.add('Title', Title, false);
+		this.state.add('Main', Main, false);
+		this.state.add('GameOver', GameOver, false);
+		this.state.add('GameWon', GameWon, false);
+
+		this.state.start('Boot');
+	}
+}
+
+let game=new Game();
